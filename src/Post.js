05abe263e8af488f06b5fcdcbefd60eb6f89371c
sanket <sanketgargate06@gmail.com>
2020-08-20 21:26:57 +0530

    added props mapping

--- src/Post.js
+++ src/Post.js
@@ -8,25 +8,27 @@ import FavouriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
 function Post({ displayName, username, verified, text, image, avatar}) {
+
     return (
         <div className="post">
             <div className="post__avatar">
-            <Avatar src={require("./assets/avatar.png")}/>
+            <Avatar src={avatar} />
             </div>
             <div className="post__body">
                 <div className="post__header">
                     <div className="post__headerText">
-                        <h3>John Doe{" "}
+                        <h3>
+                            {displayName}{" "}
                             <span className="post__headerSpecial">
-                                <VerifiedUserIcon className="post__badge" />@johndoe
+                            {verified && <VerifiedUserIcon className="post__badge" />} @{username}
                             </span>
                         </h3>
                     </div>
                     <div className="post__headerDescription">
-                        <p>Building Twitter Clone...</p>
+                        <p>{text}</p>
                     </div>
                 </div>
-                <img src={require("./assets/code.gif")} alt=""/>
+                <img src={image} alt=""/>
                 <div className="post__footer">
                     <ChatBubbleOutlineIcon fontSize="small" />
                     <RepeatIcon fontSize="small" />
